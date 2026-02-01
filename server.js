21792e3c8cd3e68ef567953b8b9419e40fa2c807
Stv <stevzgamer@gmail.com>
2026-02-01 22:43:32 +0200

    Final Render port and connection fix

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const app = express();
 // Connect to Database
 connectDB();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
